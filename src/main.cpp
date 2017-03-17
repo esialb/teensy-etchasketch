5d65dca7482c5b80b75c6fc0dd7cf23a2a0290ec
Robin Kirkman <robin.kirkman@gmail.com>
2017-03-17 01:16:31 -0400

    store images on SD card

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@
 #include <Arduino.h>
 #include <EEPROM.h>
 #include <i2c_t3.h>
+#include <SD.h>
+#include <string.h>
+#include <stdio.h>
 
 #include <t3_Adafruit_SSD1306.h>
 
@@ -39,22 +42,116 @@ void setup() {
 
 	gfx0.display();
 	gfx1.display();
+
+	if(!SD.begin(BUILTIN_SDCARD)) {
+		gfx0.print("SD error");
+	}
 }
 
-bool up() {
+bool up_key() {
 	return touchRead(23) > 1500;
 }
 
-bool right() {
+bool right_key() {
 	return touchRead(22) > 1500;
 }
 
-bool down() {
+bool down_key() {
 	return touchRead(18) > 1500;
 }
 
-bool left() {
+bool left_key() {
 	return touchRead(19) > 1500;
+}
+
+bool invert_key() {
+	return touchRead(17) > 1500;
+}
+
+bool reset_key() {
+	return touchRead(16) > 1500;
+}
+
+bool load_key() {
+	return touchRead(29) > 1500;
+}
+
+bool save_key() {
+	return touchRead(30) > 1500;
+}
+
+void save_file() {
+	char buf[20];
+	int idx = 0;
+	sprintf(buf, "%i.ets", idx);
+	while(SD.exists(buf)) {
+		idx++;
+		sprintf(buf, "%i.ets", idx);
+	}
+	File file = SD.open(buf, FILE_WRITE);
+	file.write(x);
+	file.write(y);
+	file.write(color);
+	file.write(gfx0.buffer, 1024);
+	file.write(gfx1.buffer, 1024);
+	file.close();
+}
+
+void load_file() {
+	char buf[20];
+	int idx = 0;
+	sprintf(buf, "%i.ets", idx);
+	for(; idx < 256 && !SD.exists(buf); idx++)
+		sprintf(buf, "%i.ets", idx);
+	if(idx == 256)
+		return;
+
+	gfx0.clearDisplay();
+	gfx1.clearDisplay();
+	gfx0.invertDisplay(false);
+	gfx1.invertDisplay(false);
+	while(!invert_key()) {
+		if(reset_key())
+			return;
+		if(up_key()) {
+			idx++;
+			if(idx == 256)
+				idx = 0;
+			sprintf(buf, "%i.ets", idx);
+			while(!SD.exists(buf)) {
+				idx++;
+				if(idx == 256)
+					idx = 0;
+				sprintf(buf, "%i.ets", idx);
+			}
+			while(up_key())
+				;
+		}
+		if(down_key()) {
+			idx--;
+			if(idx == -1)
+				idx = 255;
+			sprintf(buf, "%i.ets", idx);
+			while(!SD.exists(buf)) {
+				idx--;
+				if(idx == -1)
+					idx = 255;
+				sprintf(buf, "%i.ets", idx);
+			}
+			while(down_key())
+				;
+		}
+		gfx0.clearDisplay();
+		gfx0.setCursor(0, 0);
+		gfx0.print(buf);
+		gfx0.display();
+	}
+	File file = SD.open(buf, FILE_READ);
+	x = file.read();
+	y = file.read();
+	color = file.read();
+	file.read(gfx0.buffer, 1024);
+	file.read(gfx1.buffer, 1024);
 }
 
 void set(int x, int y, bool isWhite) {
@@ -65,10 +162,10 @@ void set(int x, int y, bool isWhite) {
 }
 
 void loop() {
-	bool u = up();
-	bool d = down();
-	bool l = left();
-	bool r = right();
+	bool u = up_key();
+	bool d = down_key();
+	bool l = left_key();
+	bool r = right_key();
 
 	set(x, y, blink);
 
@@ -80,12 +177,12 @@ void loop() {
 		y += (y < 0 ? 64 : 0) + (y >= 64 ? -64 : 0);
 		set(x, y, !color);
 	}
-	if(touchRead(17) > 1500) {
+	if(invert_key()) {
 		color = (color == BLACK) ? WHITE : BLACK;
 		gfx0.invertDisplay(!color);
 		gfx1.invertDisplay(!color);
 	}
-	if(touchRead(16) > 1500) {
+	if(reset_key()) {
 		x = 63;
 		y = 31;
 		color = WHITE;
@@ -97,24 +194,20 @@ void loop() {
 		gfx1.display();
 	}
 
-	if(touchRead(29) > 1500) {
-		x = EEPROM[0];
-		y = EEPROM[1];
-		color = EEPROM[2];
-		for(int i = 0; i < 1024; i++) {
-			gfx0.buffer[i] = EEPROM[i + 3];
-			gfx1.buffer[i] = EEPROM[i + 3 + 1024];
-		}
+	if(load_key()) {
+		load_file();
 		gfx0.display();
 		gfx1.display();
 		gfx0.invertDisplay(!color);
 		gfx1.invertDisplay(!color);
 	}
 
-	if(touchRead(30) > 1500) {
+	if(save_key()) {
 		if(savedelay == 0) {
 			gfx0.invertDisplay(color);
 			gfx1.invertDisplay(color);
+			save_file();
+/*
 			EEPROM.begin();
 			EEPROM.write(0, x);
 			EEPROM.write(1, y);
@@ -124,6 +217,7 @@ void loop() {
 				EEPROM.write(i + 3 + 1024, gfx1.buffer[i]);
 			}
 			EEPROM.end();
+*/
 			delay(200);
 			gfx0.invertDisplay(!color);
 			gfx1.invertDisplay(!color);
